3ce419f2f3f5b5177f8707fadc979c7c82995893
AndreyChuck <a.chucklanov@yandex.ru>
2023-03-10 22:30:24 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp4.3.7/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp4._3._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите своё имя :");
+
+            var name = Console.ReadLine();
+
+            for ( int i = name.Length-1; i >= 0; i-- )
+            {
+            Console.Write(name[i] +" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
